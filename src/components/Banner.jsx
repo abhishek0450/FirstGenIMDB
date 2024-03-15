1e258a8bea7f256c6a9fb2999f03f2303012ed8b
abhishek0450 <abhishekashyap96@gmail.com>
2024-03-16 01:17:23 +0530

    fix banner on small screen

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,8 @@
 const Banner = () => {
   return (
+    <div className="flex items-end">
     <div
-      className="md:h-[70vh] bg-cover flex items-end"
+      className="sm:h-[70vh] h-[50vh] bg-cover  bg-center w-full flex items-end"
       style={{
         backgroundImage: `url(https://npr.brightspotcdn.com/legacy/sites/ketr/files/201904/avengers_endgame_-_facebook.jpg)`,
       }}
@@ -9,6 +10,7 @@ const Banner = () => {
       <div className="text-2xl text-white bg-gray-900/60 text-center w-full pt-2 pb-2 font-sans font-serif">
         Avengers Endgame
       </div>
+    </div>
     </div>
   );
 };
