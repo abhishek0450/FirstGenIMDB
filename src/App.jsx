82c59f6e13c4494188c31a3df9bf8eed923c4284
Abhishek Kashyap <108532955+abhishek0450@users.noreply.github.com>
2024-11-17 09:21:38 +0530

    fix MovieDetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
+
+
 import Navbar from "./components/Navbar";
 import Movies from "./components/Movies";
 import Watchlist from "./components/Watchlist";
 import Banner from "./components/Banner";
 import TvShows from "./components/TvShows";
 import MovieDetails from "./components/MovieDetails";
-import MovieCard from "./components/MovieCard";
 import "./css/main.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-//importing BrowserRouter,Routes,Route from react-router-dom
-//BrowserRouter is the parent component that wraps around the entire application
-//Routes is used to define the different routes in the application
-//Route is used to define the individual route
 
 function App() {
   let [addwatchlist, setAddWatchlist] = useState([]);
-  
-  
- 
 
   let handleAddToWatchlist = (movieObj) => {
     let newWatchlist = [...addwatchlist, movieObj];
     localStorage.setItem("movieApp", JSON.stringify(newWatchlist));
     setAddWatchlist(newWatchlist);
-    
   };
+
   let handleRemoveFromWatchlist = (movieObj) => {
     let filteredWatchlist = addwatchlist.filter((movie) => {
       return movie.id != movieObj.id;
     });
     setAddWatchlist(filteredWatchlist);
     localStorage.setItem("movieApp", JSON.stringify(filteredWatchlist));
-    
   };
 
   useEffect(() => {
@@ -45,42 +35,17 @@ function App() {
     setAddWatchlist(JSON.parse(moviesFromLocalStorage));
   }, []);
 
-  let [movie, setMovie] = useState(null);
-  
-
-  const API_KEY = "789d07148ac9596cfc9028dc63928cec";
-  const MOVIE_ID = "1011985"
-  
- 
-
-  useEffect(() => {
-    const fetchMovie = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/movie/${MOVIE_ID}?api_key=${API_KEY}&language=en-US`
-        );
-        setMovie(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching movie:', error);
-      }
-    };
-
-    fetchMovie();
-  }, []);
-
   return (
     <>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
           <Route
             path="/"
             element={
               <>
                 <Banner />
-                <Movies  
+                <Movies
                   handleAddToWatchlist={handleAddToWatchlist}
                   handleRemoveFromWatchlist={handleRemoveFromWatchlist}
                   addwatchlist={addwatchlist}
@@ -88,17 +53,16 @@ function App() {
               </>
             }
           />
-
           <Route
             path="/watchlist"
-            element={<Watchlist addwatchlist={addwatchlist} setAddWatchlist={setAddWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist}/>}
+            element={<Watchlist addwatchlist={addwatchlist} setAddWatchlist={setAddWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} />}
           />
-
-          <Route path="/tvshows" element={<TvShows addwatchlist={addwatchlist} setAddWatchlist={setAddWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist}/> }/>
-
-          <Route path="/moviedetails/:id" element= {movie && <MovieDetails movie={movie} />}  />
+          <Route
+            path="/tvshows"
+            element={<TvShows addwatchlist={addwatchlist} setAddWatchlist={setAddWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} />}
+          />
+          <Route path="/moviedetails/:id" element={<MovieDetails />} />
         </Routes>
-          
       </BrowserRouter>
     </>
   );
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,55 +1,54 @@
 
-import '../css/MovieDetails.css'
 
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../css/MovieDetails.css';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const API_KEY = "789d07148ac9596cfc9028dc63928cec";
 
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+        );
+        setMovie(response.data);
+      } catch (error) {
+        console.error('Error fetching movie:', error);
+      }
+    };
 
-function MovieDetails({movie}) {
-  const { poster_path, title, overview, id, vote_average } = movie;
-  
+    fetchMovie();
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;
+
+  const { poster_path, title, overview, vote_average } = movie;
+
   return (
-
     <div className='main mt-[60px]'>
-     
-    <div  className='box poster  flex items-center justify-center rounded-lg ' >
-    <img className='rounded-3xl h-[350px]  shadow-xl shadow-black' src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-      
+      <div className='box poster flex items-center justify-center rounded-lg'>
+        <img className='rounded-3xl h-[350px] shadow-xl shadow-black' src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+      </div>
+      <div className='box title text-xl font-bold text-blue-400 flex items-center'>{title}</div>
+      <div className='box buttons text-blue-400 flex items-center'>
+        <FontAwesomeIcon icon={faHeart} size="3xl" style={{color: "#ff0000",}} />
+      </div>
+      <div className='box overview text-blue-400 flex items-center'>{overview}</div>
+      <div className='box credits text-3xl font-bold text-blue-400 flex items-center'>{vote_average}</div>
     </div>
-    <div className='box title  text-xl font-bold text-blue-400 flex items-center'>{title}</div>
-    <div className='box buttons  text-blue-400 flex items-center'>
-    <FontAwesomeIcon icon={faHeart}  size="3xl" style={{color: "#ff0000",}} />
-    </div>
-    <div className='box overview  text-blue-400 flex items-center'>{overview}</div>
-    <div className='box credits  text-3xl font-bold text-blue-400 flex items-center'>{id}</div>
-    
-    </div>
-  )
+  );
 }
 
-export default MovieDetails
+export default MovieDetails;
 
 //https://api.themoviedb.org/3/movie/1011985?api_key=789d07148ac9596cfc9028dc63928cec
 
 
-
-
-// const MovieDetails = ({ movie }) => {
-//   const { poster_path, title, overview, cast, vote_average } = movie;
-
-//   return (
-//     <div className="movie-detail">
-//       <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-//       <h2>{title}</h2>
-//       <p>{overview}</p>
-//       <p>Cast: {cast}</p>
-//       <p>Rating: {vote_average}</p>
-//       <button>Add to Favorites</button>
-//       <button>Add to Watchlist</button>
-//     </div>
-//   );
-// };
-
-// export default MovieDetails;
 
